0b0c44f2067c045f6333bbec95f3459995503ce5
Hansaja Wickrama <wickramahansaja@gmail.com>
2025-10-23 15:04:24 +0530

    Still fixing

--- SLRH/utils/api.ts
+++ SLRH/utils/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
-  timeout: 10000, // 10 seconds
+  timeout: 10000,
 });
 
 console.log("🔗 Using API base:", process.env.EXPO_PUBLIC_API_URL);
